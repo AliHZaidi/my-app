2dff139c98e4b14642d6fb341bf22f06a9abbf7a
Ali Zaidi <azaidi3@wisc.edu>
2025-08-21 11:43:30 -0500

    removing accommodations page

--- src/app/accommodations/page.tsx
+++
@@ -1,8 +0,0 @@
-import { AccommodationsUI } from "@/components/AccommodationsUI";
-import { loadDisabilityData } from "@/utils/loadDisabilities";
-
-export default async function AccommodationsPage() {
-  const disabilities = await loadDisabilityData();
-
-  return <AccommodationsUI disabilities={disabilities} />;
-}
